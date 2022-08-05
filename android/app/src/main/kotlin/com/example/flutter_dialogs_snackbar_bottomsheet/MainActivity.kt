5f70e89044c8ead08622dd73e3144c7f5a213839
carlstep <carlstep333@me.com>
2022-08-05 09:15:15 +0700

    initial starter code

---
+++ android/app/src/main/kotlin/com/example/flutter_dialogs_snackbar_bottomsheet/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_dialogs_snackbar_bottomsheet
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [ElevatedButton(onPressed: (() {}), child: Text('Save'))],
+        ),
+      ),
+    );
+  }
+}
