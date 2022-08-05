a78f5fe6a43fcd17b8833a38783afccc51ae0a9c
carlstep <carlstep333@me.com>
2022-08-05 09:43:30 +0700

    created snackbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -21,13 +22,35 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+// SnackBar is declared as a variable 'snackBar' inside of the class, but outside of the build.
+  final snackBar = SnackBar(
+    content: Text(
+      'Successfully Saved!',
+      style: GoogleFonts.poppins(fontWeight: FontWeight.w600, fontSize: 20),
+      textAlign: TextAlign.center,
+    ),
+    duration: Duration(seconds: 2),
+    action: SnackBarAction(
+      onPressed: () {},
+      label: 'Undo',
+      textColor: Colors.amber,
+    ),
+    backgroundColor: Colors.blueGrey,
+  );
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [ElevatedButton(onPressed: (() {}), child: Text('Save'))],
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(snackBar);
+              },
+              child: Text('Save'),
+            ),
+          ],
         ),
       ),
     );
